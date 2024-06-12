418c9fc55d6bb553638a2694fd30dc64a2ee0855
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-06-11 22:00:03 -0300

    :wrench: Change generated function to a macro

--- src/copy.jl
+++ src/copy.jl
@@ -5,42 +5,37 @@
 ############################################################################################
 
 # Define the copy function for the structure `Sgp4DeepSpace`.
-@generated function Base.copy(sgp4ds::Sgp4DeepSpace{T}) where T<:Number
-    fields = fieldnames(sgp4ds)
+let
+    fields = fieldnames(Sgp4DeepSpace)
+    expressions = [
+        :(new_sgp4ds.$f = sgp4ds.$f)
+        for f in fields
+    ]
 
-    expressions = Expr[:(new_sgp4ds = Sgp4DeepSpace{$T}())]
-    sizehint!(expressions, length(fields) + 1)
-
-    @inbounds for f in fields
-        push!(expressions, :(new_sgp4ds.$f = sgp4ds.$f))
+    @eval begin
+        function Base.copy(sgp4ds::Sgp4DeepSpace{T}) where T<:Number
+            new_sgp4ds = Sgp4DeepSpace{T}()
+            $(expressions...)
+            return new_sgp4ds
+        end
     end
-
-    return :(
-        $(expressions...);
-        return new_sgp4ds
-    )
 end
 
 # Define the copy function for the structure `Sgp4Propagator`.
-@generated function Base.copy(
-    sgp4d::Sgp4Propagator{Tepoch, T}
-) where {Tepoch<:Number, T<:Number}
-    fields = fieldnames(sgp4d)
+let
+    fields = fieldnames(Sgp4Propagator)
+    expressions = [
+        :(new_sgp4d.$f = sgp4d.$f)
+        for f in fields if f != :sgp4ds
+    ]
 
-    expressions = Expr[:(new_sgp4d = Sgp4Propagator{$Tepoch, $T}())]
-    sizehint!(expressions, length(fields) + 1)
-
-    @inbounds for f in fields
-        if f != :sgp4ds
-            push!(expressions, :(new_sgp4d.$f = sgp4d.$f))
-        else
-            # `sgp4ds` is the only mutable element inside this structure.
-            push!(expressions, :(new_sgp4d.$f = copy(sgp4d.$f)))
+    @eval begin
+        function Base.copy(sgp4d::Sgp4Propagator{Tepoch, T}) where {Tepoch<:Number, T<:Number}
+            new_sgp4d = Sgp4Propagator{Tepoch, T}()
+            $(expressions...)
+            # `sgp4ds` is the only mutable field in the structure.
+            new_sgp4d.sgp4ds = copy(sgp4d.sgp4ds)
+            return new_sgp4d
         end
     end
-
-    return :(
-        $(expressions...);
-        return new_sgp4d
-    )
 end
